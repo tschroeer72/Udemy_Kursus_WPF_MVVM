f1da47bd4ee3bbf5a98fed60f07efe76c017b56a
tschroeer72 <t.schroeer@web.de>
2024-09-15 08:39:58 +0200

    LoginView und LoginViewModel

---
+++ Kursprojekt/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursprojekt.ViewModel;
+
+public class LoginViewModel:BaseViewModel
+{
+    [RelayCommand]
+    void LoginUser()
+    {
+
+    }
+}
