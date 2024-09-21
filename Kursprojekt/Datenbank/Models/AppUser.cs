efa23503dc1016b1a4e6d00dd9396e93a0407c52
tschroeer72 <t.schroeer@web.de>
2024-09-21 10:28:25 +0200

    Erste DB Modelle angelegt

---
+++ Kursprojekt/Datenbank/Models/AppUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursprojekt.Datenbank.Models;
+
+#nullable disable
+public class AppUser
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    
+    public int RoleID { get; set; }
+
+    [ForeignKey(nameof(RoleID))]
+    public Role Role { get; set; }
+}
---
+++ Kursprojekt/Datenbank/Models/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursprojekt.Datenbank.Models;
+
+#nullable disable
+public class Role
+{
+    [Key]
+    public int ID { get; set; }
+
+    [Required]
+    public string RoleName { get; set; } 
+    public string Description { get; set; }
+}
