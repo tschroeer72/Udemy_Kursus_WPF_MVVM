b0196d58e4ecb53bfa1abd3c6e5847d0361abb32
tschroeer72 <t.schroeer@web.de>
2024-09-21 11:46:10 +0200

    Seeding Data

--- Kursprojekt/Datenbank/ApplicationDBContext.cs
+++ Kursprojekt/Datenbank/ApplicationDBContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kursprojekt.Datenbank.DBServices;
+using Kursprojekt.Datenbank.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +9,24 @@ using System.Threading.Tasks;
 
 namespace Kursprojekt.Datenbank;
 
-public class ApplicationDBContext
+#nullable disable
+public class ApplicationDBContext : DbContext
 {
+    private readonly string _Constring = @"Server=(LocalDb)\MSSQLLocalDB; Database = ROOMSDB; Integrated Security = True; TrustServerCertificate=True";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_Constring);
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.SeedData();
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<Role> Roles { get; set; }
 }
---
+++ Kursprojekt/Datenbank/DBServices/ModelBuilderExtention.cs
@@ -0,0 +1,43 @@
+﻿using Kursprojekt.Datenbank.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursprojekt.Datenbank.DBServices;
+
+public static class ModelBuilderExtention
+{
+    public static void SeedData(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>().HasData(new Role 
+        {
+            ID = 1, 
+            RoleName = RoleType.Admin.ToString()
+        });
+
+        string strPW = "123";
+        using var sha256 = SHA256.Create();
+        var PWByte = Encoding.Default.GetBytes(strPW);
+        var PWByteSHA = sha256.ComputeHash(PWByte);
+        var strPassword = Convert.ToBase64String(PWByteSHA);
+
+        modelBuilder.Entity<AppUser>().HasData(new AppUser 
+        {
+            ID = 1, 
+            FirstName = "Thorsten",
+            LastName = "Schröer",
+            Email = "t.schroeer@web.de",
+            Password = strPassword,
+            RoleID = 1
+        });
+
+        foreach (var fkey in modelBuilder.Model.GetEntityTypes().SelectMany(s => s.GetForeignKeys()))
+        {
+            fkey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
