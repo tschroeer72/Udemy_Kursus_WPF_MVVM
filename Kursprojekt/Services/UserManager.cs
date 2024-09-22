f0f3fc654c5f9b5a1be287fb98d733ae04f809f9
tschroeer72 <t.schroeer@web.de>
2024-09-22 09:34:20 +0200

    Anfang UserManager

---
+++ Kursprojekt/Services/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Kursprojekt.Datenbank.DBServices;
+using Kursprojekt.Datenbank.DTOs;
+using Kursprojekt.Datenbank.Models;
+
+namespace Kursprojekt.Services;
+
+public class UserManager
+{
+    private static void HashUserPassword(ref AppUser appUser)
+    {
+        string strPW = appUser.Password;
+        using var sha256 = SHA256.Create();
+        var PWByte = Encoding.Default.GetBytes(strPW);
+        var PWByteSHA = sha256.ComputeHash(PWByte);
+        appUser.Password = Convert.ToBase64String(PWByteSHA);
+    }
+
+    private static async Task<DBResponse> RegisterUser(AppUser appUser)
+    {
+        var user = await DBUnit.User.GetFirstOrDefaultAsync(filter: f => f.Email == appUser.Email);
+        if (user != null)
+        {
+            return new DBResponse() { Message = $"{user.Email} ist bereits vorhanden!" };
+        }
+        
+        HashUserPassword(ref appUser);
+        appUser.Role = null;
+        return await DBUnit.User.AddAsync((appUser));
+    }
+}
